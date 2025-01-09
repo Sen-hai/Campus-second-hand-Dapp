a7d07b64844cabd7bb22fcbc0c6ea87210e488ff
Linke <129589660+Sen-hai@users.noreply.github.com>
2025-01-09 11:36:16 +0800

    Delete Campus second-hand Dapp directory

--- Campus second-hand Dapp/React/src/contract/campusMarket.js
+++
@@ -1,9 +0,0 @@
-import { Web3 } from 'web3';
-export const web3 = new Web3(window.ethereum);
-
-export const campusMarketAddress = '';
-
-export const campusMarketABI = [];
-export const campusMarketContract = new web3.eth.Contract(campusMarketABI, campusMarketAddress);
-
-
